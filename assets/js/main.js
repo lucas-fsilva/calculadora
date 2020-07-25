a30810a68fb0beb3edda55daa5af7cd8764ec85e
Lucas <lucas.fsilva879@gmail.com>
2020-07-24 21:27:41 -0300

    alteraçao conta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-
 var display = document.querySelector('.display')
-
+var n1 = ''
+var n2 = ''
+var operator = null
 document.addEventListener('click', function (e) {
     const el = e.target;
     if (el.classList.contains('btn-num')) {
-        btnForDisplay(el.innerText)
+        n2 += el.innerText
+        addToDisplay(n2)
     }
     if (el.classList.contains('btn-clear')) {
         clearDisplay()
@@ -12,25 +14,49 @@ document.addEventListener('click', function (e) {
     if (el.classList.contains('btn-del')) {
         clearOne()
     }
-    if (el.classList.contains ('btn-eq')){
+    if (el.classList.contains('btn-eq')) {
         result()
+    }
+    if (el.classList.contains('btn-operator')) {
+        addOperator(el.innerText)
     }
 });
 
-function btnForDisplay(valor) {
-    display.value += valor
+function addToDisplay(value) {
+    display.value = value
 }
 
 function clearDisplay() {
     display.value = ' '
+    n1 =''
+    n2= ''
 }
 function clearOne() {
     display.value = display.value.slice(0, -1)
 }
 
-function result(){
-    let conta = display.value;
-    conta = eval(conta)
-    display.value = conta;
-    
+function result() {
+    n1 = parseInt(n1)
+    n2 = parseInt(n2)
+    var result
+    if (operator === '+') {
+        result = n1 + n2
+    }
+    if (operator === '-') {
+        result = n1 - n2
+    }
+    if (operator === '/') {
+        result = n1 / n2
+    }
+    if (operator === '*') {
+        result = n1 * n2
+    }
+    addToDisplay(result)
+}
+
+function addOperator(op) {
+    operator = op
+    n1 = n2
+    n2 = ''
+    addToDisplay(operator)
 }
